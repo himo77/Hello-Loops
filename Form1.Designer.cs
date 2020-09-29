facd70a01435e6812af207b138f96e68514ea662
himo77 <56316743+himo77@users.noreply.github.com>
2020-09-29 12:57:40 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Hello_Loops
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.btnDoWhile = new System.Windows.Forms.Button();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.trkBarLoop = new System.Windows.Forms.TrackBar();
+            this.lblMessageBox = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trkBarLoop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Location = new System.Drawing.Point(48, 61);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(274, 83);
+            this.btnWhile.TabIndex = 0;
+            this.btnWhile.Text = "While Loop";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // btnDoWhile
+            // 
+            this.btnDoWhile.Location = new System.Drawing.Point(379, 61);
+            this.btnDoWhile.Name = "btnDoWhile";
+            this.btnDoWhile.Size = new System.Drawing.Size(274, 83);
+            this.btnDoWhile.TabIndex = 1;
+            this.btnDoWhile.Text = "Do While Loop";
+            this.btnDoWhile.UseVisualStyleBackColor = true;
+            this.btnDoWhile.Click += new System.EventHandler(this.btnDoWhile_Click);
+            // 
+            // btnFor
+            // 
+            this.btnFor.Location = new System.Drawing.Point(760, 61);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(325, 83);
+            this.btnFor.TabIndex = 2;
+            this.btnFor.Text = "For Loop";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // trkBarLoop
+            // 
+            this.trkBarLoop.Location = new System.Drawing.Point(48, 209);
+            this.trkBarLoop.Maximum = 5;
+            this.trkBarLoop.Name = "trkBarLoop";
+            this.trkBarLoop.Size = new System.Drawing.Size(1037, 90);
+            this.trkBarLoop.TabIndex = 3;
+           
+            // 
+            // lblMessageBox
+            // 
+            this.lblMessageBox.AutoSize = true;
+            this.lblMessageBox.Location = new System.Drawing.Point(1106, 209);
+            this.lblMessageBox.Name = "lblMessageBox";
+            this.lblMessageBox.Size = new System.Drawing.Size(0, 25);
+            this.lblMessageBox.TabIndex = 4;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(351, 324);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(190, 25);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status shown here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1196, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblMessageBox);
+            this.Controls.Add(this.trkBarLoop);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.btnDoWhile);
+            this.Controls.Add(this.btnWhile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trkBarLoop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button btnDoWhile;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.TrackBar trkBarLoop;
+        private System.Windows.Forms.Label lblMessageBox;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_Loops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            DialogResult selected = MessageBox.Show("Do you want to see some MessageBox dialogs?", "While", MessageBoxButtons.YesNo);
+
+            while (selected == DialogResult.Yes)
+            {
+                selected = MessageBox.Show("Here's a MessageBox. Do you want another?", "while", MessageBoxButtons.YesNo);
+            }
+        }
+
+        private void btnDoWhile_Click(object sender, EventArgs e)
+        {
+            DialogResult selected;
+
+            do
+            {
+                selected = MessageBox.Show("Do you want to see some MessageBox dialogs?", "Do While", MessageBoxButtons.YesNo);
+            } while (selected == DialogResult.Yes);
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            int value = this.trkBarLoop.Value;
+            for (int i = 0; i < value; i++)
+            {
+                MessageBox.Show("Here's MessageBox " + i);
+            }
+
+        }
+
+       
+    }
+}
+       
